84bb0786bc3556db177bde75767e944f794d44e9
Jared Henderson <jared@netrivet.com>
2022-03-02 10:45:12 -0500

    SAVEPOINT

--- actions/should-republish-evans/index.ts
+++ actions/should-republish-evans/index.ts
@@ -2,21 +2,22 @@ import fs from 'fs';
 import fetch from 'cross-fetch';
 import isEqual from 'lodash.isequal';
 import * as core from '@actions/core';
-import EVANS_BUILD_QUERY from '@friends-library/evans';
+import { QUERY, sortFriends } from '@friends-library/evans';
 import * as cloud from '@friends-library/cloud';
 import log from '@friends-library/slack';
 
 // type Metadata = { size: number; hash: string };
+type Friend = ReturnType<typeof sortFriends>[number];
 
 async function main(): Promise<void> {
-  const buildData = await getApiBuildData();
-  const lastBuildMeta = await getLastBuildMeta();
-  if (!buildData || !lastBuildMeta) {
+  const apiFriends = await getApiFriends();
+  const lastBuildFriends = await getLastBuildFriends();
+  if (!apiFriends || !lastBuildFriends) {
     return;
   }
 
   // const hash = md5(JSON.stringify(buildData));
-  if (isEqual(buildData, lastBuildMeta)) {
+  if (isEqual(apiFriends, lastBuildFriends)) {
     log.debug(`No republish of evans websites needed`);
     core.setOutput(`should_republish`, `false`);
     core.info(`Output \`should_republish\` set to \`false\``);
@@ -24,7 +25,7 @@ async function main(): Promise<void> {
     log.info(`Republish of evans websites needed`);
     core.setOutput(`should_republish`, `true`);
     const tmpPath = `${process.cwd()}/data.json`;
-    fs.writeFileSync(tmpPath, JSON.stringify(buildData));
+    fs.writeFileSync(tmpPath, JSON.stringify(apiFriends));
     await cloud.uploadFile(tmpPath, CLOUD_FILEPATH);
     // cloud.up
     // core.info(`Output \`should_republish\` set to \`true\``);
@@ -34,7 +35,7 @@ async function main(): Promise<void> {
   }
 }
 
-async function getLastBuildMeta(): Promise<Record<string, unknown> | null> {
+async function getLastBuildFriends(): Promise<Friend[] | null> {
   try {
     const buffer = await cloud.downloadFile(CLOUD_FILEPATH);
     return JSON.parse(buffer.toString());
@@ -44,7 +45,7 @@ async function getLastBuildMeta(): Promise<Record<string, unknown> | null> {
   }
 }
 
-async function getApiBuildData(): Promise<Record<string, unknown> | null> {
+async function getApiFriends(): Promise<Friend[] | null> {
   const endpoint = process.env.INPUT_FLP_API_ENDPOINT;
   try {
     const res = await fetch(endpoint ?? ``, {
@@ -53,15 +54,17 @@ async function getApiBuildData(): Promise<Record<string, unknown> | null> {
         Authorization: `Bearer ${process.env.INPUT_FLP_API_TOKEN}`,
         'Content-Type': `application/json`,
       },
-      body: JSON.stringify({ query: EVANS_BUILD_QUERY }),
+      body: JSON.stringify({ query: QUERY }),
     });
-    const json: { data?: Record<string, any>; errors?: Array<{ message: string }> } =
-      await res.json();
-    if (json.errors) {
+    const json: {
+      data?: { friends: Friend[] };
+      errors?: Array<{ message: string }>;
+    } = await res.json();
+    if (json.errors || !json.data) {
       reportError(`Got errors fetching evans build data: ${JSON.stringify(json.errors)}`);
       return null;
     }
-    return json;
+    return sortFriends(json.data.friends);
   } catch (error: unknown) {
     reportError(`Error fetching evans build data: ${error}`);
     return null;
