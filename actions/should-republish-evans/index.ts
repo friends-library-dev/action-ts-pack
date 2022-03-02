9a963c31a4f1208ee76b42bd3abdcf94cc16cefd
Jared Henderson <jared@netrivet.com>
2022-03-02 08:32:10 -0500

    SAVEPOINT

--- actions/should-republish-evans/index.ts
+++ actions/should-republish-evans/index.ts
@@ -1,11 +1,12 @@
+import fs from 'fs';
 import fetch from 'cross-fetch';
-import md5 from 'md5';
+import isEqual from 'lodash.isequal';
 import * as core from '@actions/core';
 import EVANS_BUILD_QUERY from '@friends-library/evans';
 import * as cloud from '@friends-library/cloud';
 import log from '@friends-library/slack';
 
-type Metadata = { size: number; hash: string };
+// type Metadata = { size: number; hash: string };
 
 async function main(): Promise<void> {
   const buildData = await getApiBuildData();
@@ -14,22 +15,26 @@ async function main(): Promise<void> {
     return;
   }
 
-  const hash = md5(JSON.stringify(buildData));
-  if (buildData.length === lastBuildMeta.size && hash === lastBuildMeta.hash) {
+  // const hash = md5(JSON.stringify(buildData));
+  if (isEqual(buildData, lastBuildMeta)) {
     log.debug(`No republish of evans websites needed`);
     core.setOutput(`should_republish`, `false`);
     core.info(`Output \`should_republish\` set to \`false\``);
   } else {
     log.info(`Republish of evans websites needed`);
     core.setOutput(`should_republish`, `true`);
-    core.info(`Output \`should_republish\` set to \`true\``);
-    const newMeta: Metadata = { size: buildData.length, hash };
-    core.setOutput(`build_meta`, JSON.stringify(newMeta));
-    core.info(`Output \`build_meta\` set to \`${JSON.stringify(newMeta)}\``);
+    const tmpPath = `${process.cwd()}/data.json`;
+    fs.writeFileSync(tmpPath, JSON.stringify(buildData));
+    await cloud.uploadFile(tmpPath, CLOUD_FILEPATH);
+    // cloud.up
+    // core.info(`Output \`should_republish\` set to \`true\``);
+    // const newMeta: Metadata = { size: buildData.length, hash };
+    // core.setOutput(`build_meta`, JSON.stringify(newMeta));
+    // core.info(`Output \`build_meta\` set to \`${JSON.stringify(newMeta)}\``);
   }
 }
 
-async function getLastBuildMeta(): Promise<Metadata | null> {
+async function getLastBuildMeta(): Promise<Record<string, unknown> | null> {
   try {
     const buffer = await cloud.downloadFile(CLOUD_FILEPATH);
     return JSON.parse(buffer.toString());
